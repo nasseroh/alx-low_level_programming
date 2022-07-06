a1c8be9246627d9e71fc75401adfcc732113e4ea
Your Name <you@example.com>
2022-07-06 06:21:38 -0700

    changes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet : prints the lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	int N;
+	for(N = 'a'; N <='z'; N++)
+	{
+		_putchar(N);
+	}
+	_puchar("\n");
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int _putchar(char N);
+void print_alphabet(void);
