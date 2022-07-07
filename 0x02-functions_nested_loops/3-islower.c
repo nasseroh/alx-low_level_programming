8db4ac34207678c0de7c3c8d2f0bcddea4e8ea32
Your Name <you@example.com>
2022-07-06 18:40:39 -0700

    changes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 
 /**
  *  _islower - checks the character if it is lowercase
- * @n : a character argument
+ * @c : a character argument
  * Return: returns 1 and 0 depending on condition
  */
 
-int _islower(int n)
+int _islower(int c)
 {
-	return (n >= 'a' && n <= 'z');
+	return (c >= 'a' && c <= 'z');
 }
