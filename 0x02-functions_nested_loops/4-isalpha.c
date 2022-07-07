c75abf651d36242f5cf1e3c94324e26729fb1dc5
Your Name <you@example.com>
2022-07-06 19:01:57 -0700

    some changes

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,10 @@
 
 /**
  * _isalpha - checks for alphabetic characters
- * @c: a character to be check on 
- * Return: returns 0 or 1 depending on the condition 
+ * @c: a character to be check on
+ * Return: returns 0 or 1 depending on the condition
  */
+
 int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
