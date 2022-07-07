124eabafe7589851b217b391ecbcbbf6ad5bacab
Your Name <you@example.com>
2022-07-06 19:14:33 -0700

    some changes

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign based on condition
+ * @n: argument passed
+ * Return: returnts 1, 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
